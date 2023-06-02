9ee03da461136b054ef54f2ff3cbdac3833057c7
broman9107 <129743012+broman9107@users.noreply.github.com>
2023-06-02 07:17:17 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,59 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let arr2 = [];
+let target = 9;
+let start = 0;
+let end = arr.length - 1;
+
+for (i = 1; i <= 5000000; i++) {
+    arr2.push(i);
+}
+
+
+let end2 = arr2.length - 1;
+
+function constant(n) {
+    return n;
+}
+
+function linear(arr, target) {
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return arr[i];
+        };
+    }
+    return false;
+}
+
+function quadratic(n) {
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            console.log("Hey");
+        }
+    }
+}
+
+function logn(n) {
+    while (n > 1) {
+        n = Math.floor(n / 2);
+        console.log(n)
+    }
+}
+
+function binarySearchRecursive(arr, start, end, target) {
+    if (start > end) {
+        return false
+    };
+    let midIndex = Math.floor((start + end) / 2);
+    console.log(midIndex)
+    if (arr[midIndex] === target) {
+        return arr[midIndex];
+    } else {
+        if (arr[midIndex] > target) {
+            return binarySearchRecursive(arr, start, midIndex - 1, target);
+        } else {
+            return binarySearchRecursive(arr, midIndex + 1, end, target);
+        }
+    }
+}
+
+console.log(binarySearchRecursive(arr2, start, end2, 459621))
