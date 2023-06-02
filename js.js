fc542d5a2da711ae09775d3e49e4a36ea9cbd0b0
bromanguy9107 <trentonwilliams9107@yahoo.com>
2023-06-02 09:15:45 -0400

    Updated Everything

--- js.js
+++ js.js
@@ -1,26 +1,40 @@
+let linearTarget = document.getElementById("linear-target");
+let linearArrSize = document.getElementById("linear-array-size");
+let linearSubmit = document.getElementById("linear-submit");
+let consoleLogging = document.getElementById("console-logging");
+
+linearSubmit.onclick = () => {
+  if (linearArrSize.value === "" || linearTarget.value === "") {
+    alert("No Values");
+    return;
+  }
+  fillArray(linearArrSize.value);
+  consoleLogging.innerHTML = linear(arr2, parseInt(linearTarget.value));
+};
+
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let arr2 = [];
 let target = 9;
 let start = 0;
 let end = arr.length - 1;
 
-for (i = 1; i <= 5000000; i++) {
-  arr2.push(i);
+function fillArray(input) {
+  for (i = 1; i <= input; i++) {
+    arr2.push(i);
+  }
 }
-
-let end2 = arr2.length - 1;
 
 function constant(n) {
   return n;
 }
 
-function linear(arr, target) {
+function linear(arr, target) { 
   for (i = 0; i < arr.length; i++) {
     if (arr[i] === target) {
       return arr[i];
     }
   }
-  return false;
+  return "Target not found";
 }
 
 function quadratic(n) {
@@ -56,11 +70,11 @@ function binarySearchRecursive(arr, start, end, target) {
 }
 
 function binarySearchWhile(arr, target) {
-    let start = 0;
-    let end = arr.length - 1;
+  let start = 0;
+  let end = arr.length - 1;
   while (start <= end) {
     let midIndex = Math.floor((start + end) / 2);
-    console.log(midIndex)
+    console.log(midIndex);
     if (arr[midIndex] === target) {
       return arr[midIndex];
     } else if (arr[midIndex] > target) {
@@ -71,5 +85,3 @@ function binarySearchWhile(arr, target) {
   }
   return "Could not find target";
 }
-
-
