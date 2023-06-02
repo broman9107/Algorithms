02795fc0622cbf59dd4176015e26f5e4bd4d2047
bromanguy9107 <trentonwilliams9107@yahoo.com>
2023-06-02 11:50:11 -0400

    Changed Everything

--- js.js
+++ js.js
@@ -1,37 +1,56 @@
 let linearTarget = document.getElementById("linear-target");
 let linearArrSize = document.getElementById("linear-array-size");
 let linearSubmit = document.getElementById("linear-submit");
-let consoleLogging = document.getElementById("console-logging");
-
-linearSubmit.onclick = () => {
-  if (linearArrSize.value === "" || linearTarget.value === "") {
-    alert("No Values");
-    return;
-  }
-  consoleLogging.innerHTML = linear(fillArray(parseInt(linearArrSize.value)), parseInt(linearTarget.value));
-  linearArrSize.value = "";
-  linearTarget.value = "";
-};
+let iterationDisplay = document.getElementById("iteration-display");
+let resetButton = document.getElementById("reset-button");
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let target = 9;
 let start = 0;
 let end = arr.length - 1;
 
+//------------------- On Click Functions -------------------\\
+
+resetButton.onclick = () => {
+  iterationDisplay.innerHTML = "";
+  linearArrSize.value = "";
+  linearTarget.value = "";
+};
+
+linearSubmit.onclick = () => {
+  if (linearArrSize.value === "" || linearTarget.value === "") {
+    alert("No Values");
+    return;
+  }
+  linear(
+    fillArray(parseInt(linearArrSize.value)),
+    parseInt(linearTarget.value)
+  );
+};
+
+//------------------- Fill Array -------------------\\
+
 function fillArray(input) {
-    let arr2 = [];
+  let arr2 = [];
   for (i = 1; i <= input; i++) {
-    arr2.push(i);  
+    arr2.push(i);
   }
   return arr2;
 }
 
+//------------------- Big O Types -------------------\\
+
+//Constant
 function constant(n) {
   return n;
 }
 
-function linear(arr, target) { 
+//Linear
+function linear(arr, target) {
   for (i = 0; i < arr.length; i++) {
+    iterationDisplay.innerHTML = iterationDisplay.innerHTML.concat(
+      ` ${arr[i]}`
+    );
     if (arr[i] === target) {
       return arr[i];
     }
@@ -39,6 +58,7 @@ function linear(arr, target) {
   return "Target not found";
 }
 
+//Quadratic
 function quadratic(n) {
   for (i = 0; i < n; i++) {
     for (j = 0; j < n; j++) {
@@ -47,6 +67,7 @@ function quadratic(n) {
   }
 }
 
+//LogN
 function logn(n) {
   while (n > 1) {
     n = Math.floor(n / 2);
@@ -54,6 +75,9 @@ function logn(n) {
   }
 }
 
+//------------------- Algorithms -------------------\\
+
+//Binary Search Recursive uses O(LogN)
 function binarySearchRecursive(arr, start, end, target) {
   if (start > end) {
     return false;
@@ -71,7 +95,8 @@ function binarySearchRecursive(arr, start, end, target) {
   }
 }
 
-function binarySearchWhile(arr, target) {
+//Binary Search Itterative uses O(LogN)
+function binarySearchItterative(arr, target) {
   let start = 0;
   let end = arr.length - 1;
   while (start <= end) {
